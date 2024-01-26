9187b5611b4b871d3514968d87435fdbe40cd01c
VengoVE <v726562@gmail.com>
2024-01-26 19:37:52 +0300

    Task4

---
+++ Tyuiu.VengoVE.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VengoVE.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.VengoVE.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
---
+++ Tyuiu.VengoVE.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.VengoVE.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.VengoVE.Sprint2.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Вэнго В. Э. | ИСПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Вэнго В. Э. | ИСПб-23-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, вычисляет требуемое значение с использованием       *");
+            Console.WriteLine("* тернарного оператора, где пользователь вводит значение переменных x, y  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значения переменной X");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значения переменной Y");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
